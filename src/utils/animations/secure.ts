5601b77f15e2d2ad8190688b23179d28a800a12b
Alexandr Smirnov <alexrus37reg@gmail.com>
2025-09-02 00:22:29 +0300

    fix: animation

--- src/utils/animations/secure.ts
+++ src/utils/animations/secure.ts
@@ -1,11 +1,32 @@
 
 export function animateSecure(gsap: GSAP): GSAPTimeline {
 
-  const tlGallery = gsap.timeline({});
+  const tlGallery = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".secure",
+      start: "top+=1px top",
+      scrub: 1,
+      // scrub: false,
+      // scrub: true,
 
-  tlGallery.to(
+      end: "bottom"
+      // pin: true,
+    },
+  });
+
+  gsap.to(
     ".secure__item .orbit",
     {
+      scrollTrigger: {
+        trigger: ".secure",
+        start: "top center",
+        // scrub: 1,
+        // scrub: false,
+        // scrub: true,
+
+        end: "bottom",
+        // pin: true,
+      },
       rotation: "+=360",
       duration: 5,
       // stagger: 0.35
